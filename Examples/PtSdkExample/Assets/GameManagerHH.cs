2b9bc381be2117bfeb7dd455973823359db3937a
Jiaqi Wang <jackyjqw@gmail.com>
2018-08-09 19:57:26 -0700

    sample scene init

---
+++ Examples/PtSdkExample/Assets/GameManagerHH.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayTable;
+
+public class GameManagerHH : MonoBehaviour {
+
+    private void Awake()
+    {
+        PTHandheld.Initialize();
+    }
+}
---
+++ Examples/PtSdkExample/Assets/GameManagerTT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayTable;
+
+public class GameManagerTT : MonoBehaviour {
+    public PTPlayer player;
+    public Transform playerTransform;
+
+    private void Awake()
+    {
+        PTTableTop.Initialize(player);
+
+        PTTableTop.OnConnected += (player) =>
+        {
+
+        };
+    }
+}
